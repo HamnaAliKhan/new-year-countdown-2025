d18a653be6bd18dc54ecb36f1e4bd638360c0775
HamnaAliKhan <hamnaalikhann@gmail.com>
2024-12-25 23:14:52 +0500

    Ive changed the bottom most wish a little above the footer part

--- newyear.js
+++ newyear.js
@@ -3,15 +3,13 @@ const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
 
-// Get the current year and next year
 const currentYear = new Date().getFullYear();
 const nextYear = currentYear + 1;
 
-// Set next year's New Year date explicitly in UTC
 //const nextYearDate = new Date(Date.UTC(nextYear, 0, 1, 0, 0, 0));
 const nextYearDate = new Date(new Date().getTime() + 5000); // 10 seconds from now
 
-// Helper function to format numbers to two digits
+
 const formatValue = (value) => (value < 10 ? `0${value}` : value);
 
 let timer = setInterval(() => {
@@ -30,24 +28,8 @@ let timer = setInterval(() => {
         seconds.innerHTML = formatValue(secondsLeft);
     }
 
-    // Stop countdown and redirect after hitting 0
     if (totalSeconds <= 0) {
         clearInterval(timer);
         window.location.href = 'wish.html';
     }
 }, 1000);
- //animation: moveFont 2s ease-in;
- //@keyframes moveFont {
-   // 0% {
-    //    opacity: 0;
-      //  transform: translateY(-50px) scale(0.5);
-    //}
-    //50% {
-      //  opacity: 0.5;
-        //transform: translateY(0px) scale(0.75);
-    //}
-    //100% {
-      //  opacity: 1;
-        //transform: translateY(0) scale(1);
-    //}
-//}
