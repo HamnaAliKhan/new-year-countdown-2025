16544be2d6f92479f77511d4a419477eaae38893
HamnaAliKhan <hamnaalikhann@gmail.com>
2025-01-01 00:15:20 +0500

    Fixed incorrect timer

--- newyear.js
+++ newyear.js
@@ -1,39 +1,35 @@
-const days = document.querySelector('#days');
-const hours = document.querySelector('#hours');
-const minutes = document.querySelector('#minutes');
-const seconds = document.querySelector('#seconds');
+document.addEventListener('DOMContentLoaded', () => {
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
 
-const currentYear = new Date().getFullYear();
-const nextYear = currentYear + 1;
-
-const nextYearDate = new Date(Date.UTC(nextYear, 0, 1, 0, 0, 0));
-
-
-
-const formatValue = (value) => (value < 10 ? `0${value}` : value);
-
-let timer = setInterval(() => {
-    const currentDate = new Date();
-
-    const currentDateUTC = Date.UTC(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
-
-    const totalSeconds = Math.floor((nextYearDate - currentDateUTC) / 1000);
-
-    const daysLeft = Math.floor(totalSeconds / 3600 / 24);
-    const hoursLeft = Math.floor((totalSeconds % (3600 * 24)) / 3600);
-    const minutesLeft = Math.floor((totalSeconds % 3600) / 60);
-    const secondsLeft = totalSeconds % 60;
-
-
-    if (days && hours && minutes && seconds) {
-        days.innerHTML = formatValue(daysLeft);
-        hours.innerHTML = formatValue(hoursLeft);
-        minutes.innerHTML = formatValue(minutesLeft);
-        seconds.innerHTML = formatValue(secondsLeft);
+    const nextYearDate = new Date(2025, 0, 1, 0, 0, 0);
+    if(new Date()>nextYearDate){
+        window.location.href = 'wish.html';
     }
 
-    if (totalSeconds <= 0) {
-        clearInterval(timer);
-        window.location.href = 'wish.html';
-    }
-}, 1000);
+    const formatValue = (value) => (value < 10 ? `0${value}` : value);
+
+    let timer = setInterval(() => {
+        const currentDate = new Date();
+        const totalSeconds = Math.floor((nextYearDate - currentDate) / 1000);
+
+        const daysLeft = Math.floor(totalSeconds / 3600 / 24);
+        const hoursLeft = Math.floor((totalSeconds % (3600 * 24)) / 3600);
+        const minutesLeft = Math.floor((totalSeconds % 3600) / 60);
+        const secondsLeft = totalSeconds % 60;
+
+        if (days && hours && minutes && seconds) {
+            days.innerHTML = formatValue(daysLeft);
+            hours.innerHTML = formatValue(hoursLeft);
+            minutes.innerHTML = formatValue(minutesLeft);
+            seconds.innerHTML = formatValue(secondsLeft);
+        }
+
+        if (totalSeconds <= 0) {
+            clearInterval(timer);
+            window.location.href = 'wish.html';
+        }
+    }, 1000);
+});
